78c7ce1f7c418aeb899e786aaaf612751a031c40
Welly DIALLO <diallowelly620@gmail.com>
2026-06-14 19:29:53 +0000

    creéation du portfolio

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Bienvenue sur mon Portfolio")
+st.write("Je suis Welly, étudiante en IA & Big Data.")
+
+st.header("Mes projets")
+st.write("1. Projet Titanic (Python & ML)")
+st.write("2. Analyse de données SQL")
